fb64a15a6c4d3cbeee7deb006f59c1c5e7695ade
SantiagoFer <49788541+SantiagoFer@users.noreply.github.com>
2019-10-08 17:06:15 -0400

    Refined jump still in progress, incorporating velocity.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -14,12 +14,14 @@ public class PlayerMovement : MonoBehaviour
 //jump
     public bool jumpFlag;
 
+    float maxDist = 0.4f;
+
     public Vector3 velocity;
     public float startVel;
     Rigidbody rb;
     AudioSource source;
     Vector3 defPos;
-    bool onFloor;
+    public bool onFloor;
     public float jumpSpd;
     public float grav = .01f;
     public float xAccel;
@@ -37,17 +39,12 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //  if (Input.GetKey(KeyCode.Space))
-        // {
-        //     jumpFlag = true;
-
-        // }
-
-           if (Input.GetKeyDown(KeyCode.Space) && jumpFlag)
+         if (Input.GetKey(KeyCode.Space))
         {
-            rb.AddForce(new Vector3(0f, 3f, 0f), ForceMode.Impulse);
-            jumpFlag = false;
+            jumpFlag = true;
+
         }
+           
     }
 
     void OnCollisionStay(){
@@ -57,19 +54,40 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         //Jump stuff
-        // BoxCollider box = GetComponent<BoxCollider>();
-        // Vector3 myPos = transform.position;
-        // Vector3 leftPoint = myPos + (Vector3.left* (box.size.x /2f));
-        // RaycastHit ray = Physics.Raycast(leftPoint,Vector3.down, (box.size.y/2f)+ 0.1f, LayerMask.GetMask("Floor"));
-        // Vector3 rightPoint = myPos + (Vector3.right* (box.size.x /2f));
-        // RaycastHit ray2 = Physics.Raycast(rightPoint, Vector3.down, (box.size.y/2f)+ 0.1f, LayerMask.GetMask("Floor"));
+        BoxCollider box = GetComponent<BoxCollider>();
+        Vector3 myPos = this.transform.position;
 
-        // if (ray || ray2)
-        //     onFloor = true;
-        // else {
-        //     onFloor = false;
-        //     velocity.y -= grav;
-        // }
+        Ray rayLeft = new Ray (myPos + (Vector3.left) * (box.size.x /2f), Vector3.down);
+        Ray rayRight = new Ray (myPos + (Vector3.right) * (box.size.x /2f), Vector3.down);
+
+        Debug.DrawRay(myPos + (Vector3.left) * (box.size.x /2f), Vector3.down, Color.cyan, maxDist);
+        Debug.DrawRay(myPos + (Vector3.right) * (box.size.x /2f), Vector3.down, Color.cyan, maxDist);
+
+
+        RaycastHit ray = new RaycastHit();
+        RaycastHit ray2 = new RaycastHit();
+
+
+        if ( Physics.Raycast(rayLeft, out ray, 0.51f)|| Physics.Raycast(rayRight, out ray2, 0.51f))
+        {
+            Debug.Log("ground");
+            onFloor = true;
+        }else{
+            onFloor= false; 
+            velocity.y -= grav;
+        }
+
+      
+       
+        if ( onFloor == true && jumpFlag == true){
+
+            if (Input.GetKey(KeyCode.Space)){
+                jumpFlag = false;
+                velocity += Vector3.up * jumpSpd;
+            }
+        }
+
+
 
 
         if(Input.GetKey(KeyCode.D))
@@ -82,6 +100,7 @@ public class PlayerMovement : MonoBehaviour
             this.transform.Translate(new Vector3(-moveSpeed, 0,0  ));
 
         }
+      
 
      
 
@@ -89,3 +108,18 @@ public class PlayerMovement : MonoBehaviour
 
     }
 }
+
+
+  // Vector3 leftPoint = myPos + (Vector3.left* (box.size.x /2f));
+        // RaycastHit ray = Physics.Raycast(leftPoint,Vector3.down, (box.size.y/2f)+ 0.1f, LayerMask.GetMask("Floor"));
+        // Vector3 rightPoint = myPos + (Vector3.right* (box.size.x /2f));
+        // RaycastHit ray2 = Physics.Raycast(rightPoint, Vector3.down, (box.size.y/2f)+ 0.1f, LayerMask.GetMask("Floor"));
+
+        // if (ray || ray2)
+        //     onFloor = true;
+        // else {
+        //     onFloor = false;
+        //     velocity.y -= grav;
+        // }
+
+        //movement mods
